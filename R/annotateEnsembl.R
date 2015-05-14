0db9a71a6dcea4c085faa5c3df1ed132231d3c7e
ttriche <tim.triche@gmail.com>
2015-05-13 20:20:17 -0700

    I can't figure why splitting a GRanges is so slow

--- R/annotateEnsembl.R
+++ R/annotateEnsembl.R
@@ -4,6 +4,8 @@
 #' @param tpm a matrix of transcripts per million estimates
 #' @param txome a character string naming the txome, e.g. "EnsDb.Hsapiens.v79"
 #'
+#' @value a list with a SummarizedExperiment, a TPM matrix, and a txome string
+#'
 annotateEnsembl <- function(tpm, txome) {
 
   if (!grepl("EnsDb", txome)) stop("You must specify an ENSEMBL transcriptome")
@@ -13,10 +15,11 @@ annotateEnsembl <- function(tpm, txome) {
   txmap <- transcripts(get(txome), columns=c("tx_id","tx_biotype","entrezid"))
   txmap <- txmap[which(txmap$tx_id %in% rownames(tpm) & txmap$entrezid != "")]
   txmap <- txmap[!grepl(";", txmap$entrezid)] ## toss out multi-mapped ENSGs
+  names(txmap) <- txmap$tx_id ## so that the GRangesList makes sense later
   mapByGene <- function(x) tapply(x[txmap$tx_id], txmap$entrezid, sum)
-  tpmByGene <- SummarizedExperiment(SimpleList(tpm=apply(tpm, 2, mapByGene)),
-                                    rowRanges=split(txmap, txmap$entrezid))
-
+  GRList <- GRangesList(split(txmap, txmap$entrezid)) ## why is this so slow?!
+  tpmByGene <- apply(tpm, 2, mapByGene) ## this is fast as hell by comparison 
+  SummarizedExperiment(assays=SimpleList(tpm=tpmByGene), rowRanges=GRList)
   results <- list(tpmByGene=tpmByGene, tpmByTranscript=tpm, txome=txome)
   return(results)
 
