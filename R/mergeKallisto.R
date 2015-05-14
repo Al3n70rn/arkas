941dd8df7ceba034e5dfe63f376cf637fc51fa58
ttriche <tim.triche@gmail.com>
2015-05-13 17:12:18 -0700

    begin semantic versioning, fix a function name

--- R/mergeKallisto.R
+++ R/mergeKallisto.R
@@ -1,5 +1,5 @@
 ## merge several samples' results from Kallisto 
-mergeKallistoForArtemis <- function(mergePath, txome="EnsDb.Hsapiens.v79", ...){
+mergeKallisto <- function(mergePath, txome="EnsDb.Hsapiens.v79", ...){
   
   h5Files <- list.files(mergePath, pattern=".h5$")
   processed <- mclapply(h5Files, fetchKallisto) ## don't annotate yet
