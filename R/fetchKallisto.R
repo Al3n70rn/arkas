ae1873ccb4a3396f24c70c2874ab3697d71b70a8
ttriche <tim.triche@gmail.com>
2015-05-19 13:01:15 -0700

    much cleaner, faster bundling of results
    Former-commit-id: 40e55246ffdf1cc6286378857da729acf0d57e17

--- R/fetchKallisto.R
+++ R/fetchKallisto.R
@@ -5,11 +5,42 @@
 #' @import jsonlite 
 #' @import matrixStats
 #'
+#' @param path            character string: the path to h5/json files 
 #' @param h5file          character string: the file to read
 #' @param checkRunInfo    boolean: check run_info.json against the hdf5 call?
 #'
-fetchKallisto <- function(hdf5File, checkRunInfo=TRUE, ...) {
+fetchKallisto <- function(path=".", h5file="abundance.h5", checkRunInfo=TRUE) {
 
+  hdf5 <- paste0(path.expand(path), "/", h5file) 
+  bootstraps <- h5read(hdf5, "aux/num_bootstrap")
+  ## if bootstraps are found, summarize them...
   
+  if (bootstraps > 0) { 
+    message("Found ", bootstraps, " bootstraps for ", path, ", summarizing...")
+    # grab a column vector for each bootstrap, then bind them all into a Matrix
+    bsidx <- paste0("bootstrap/bs", seq(0, bootstraps - 1))
+    boots <- do.call(cbind, lapply(bsidx, function(bs) h5read(hdf5, bs)))
+    res <- Matrix(cbind(rowMedians(boots), 
+                        rowMads(boots),
+                        h5read(hdf5, "aux/eff_lengths")),
+                  dimnames=list(transcript=h5read(hdf5, "aux/ids"),
+                                c("est_count", "eff_length", "est_count_mad")))
+  } else {
+    message("No bootstraps found for ", path, ", using recorded est_counts...")
+    res <- Matrix(cbind(h5read(hdf5, "est_counts"),
+                        h5read(hdf5, "aux/eff_lengths")),
+                  dimnames=list(transcript=h5read(hdf5, "aux/ids"),
+                                c("est_count", "eff_length")))
+  }
+
+  ## ensure the information in run_info.json matches the hdf5 call
+  if (checkRunInfo) {
+    runinfo <- fetchRunInfo(sub("abundance.h5", "run_info.json", hdf5))
+    if (runinfo$call != h5read(hdf5, "aux/call")) {
+      stop("JSON run_info does not match hdf5 file! Something is likely wrong.")
+    }
+  }
+
+  return(res)
 
 }
--- R/mergeKallisto.R
+++ R/mergeKallisto.R
@@ -1,19 +1,36 @@
-#' merge several samples' results from Kallisto 
+#' merge multiple Kallisto results, by default yielding a SummarizedExperiment 
 #'
-#' @param mergePath where the files are 
-#' @param txome a character string specifying the target Ensembl transcriptome
+#' @param resultDirs  character vector: directory names holding Kallisto results
+#' @param basePath    character string: base path to resultPaths, default "."
+#' @param txomes      character vector: the target transcriptome(s)/repeatome(s)
+#' @param value       character string: return a SummarizedExperiment or a list?
 #'
-mergeKallisto <- function(mergePath, txome="EnsDb.Hsapiens.v79", ...){
+mergeKallisto <- function(resultDirs,
+                          basePath=".",
+                          txomes=c(), 
+#                         txomes=c("ERCC", 
+#                                  "EnsDb.Hsapiens.v79", 
+#                                  "RepBase.Hsapiens.20_04"),
+                          value=c("SummarizedExperiment", "list"), ...) {
+ 
+  targets <- paste0(path.expand(basePath), "/", resultDirs)
+  stopifnot(all(targets %in% list.dirs(basePath)))
+  names(targets) <- resultDirs
+  value <- match.arg(value)
+
+  res <- mclapply(targets, fetchKallisto)
+  cols <- do.call(rbind, lapply(res, colnames))
+  if (!all(apply(cols, 2, function(x) length(unique(x)) == 1))) {
+    stop("Some of your Kallisto runs have bootstraps and some don't. Exiting.")
+  } 
+  cnames <- apply(cols, 2, unique)
+  names(cnames) <- cnames
+  asys <- lapply(cnames, function(x) do.call(cbind, lapply(res, `[`, j=x)))
   
-  h5Files <- paste0(mergePath, "/", list.files(mergePath, pattern=".h5$"))
-  names(h5Files) <- sub(paste0(mergePath, "/"), "", sub(".h5$", "", h5Files))
-  res <- mclapply(h5Files, fetchKallisto)
-  res <- list(counts=do.call(cbind, lapply(res, `[[`, "counts")),
-              efflen=do.call(cbind, lapply(res, `[[`, "efflen")))
-  if (!is.null(txome)) {
-    res <- annotateEnsembl(res, txome)
-  } else {
-    res <- list(tpm=res$counts / res$efflen)
+  if (value == "SummarizedExperiment") {
+    res <- SummarizedExperiment(assays=asys, 
+                                metadata=list(source="Kallisto (via Artemis)"))
+    if(length(txomes) > 0) res <- annotateBundles(res, txomes)
   }
   return(res)
 
