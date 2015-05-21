2f0a92cafe606426ffd2a34966ae4bd7aa7c4640
ttriche <tim.triche@gmail.com>
2015-05-20 18:46:45 -0700

    fix a bug in ERCC annotation and bump the version
    Former-commit-id: c45b906aaf398a379e72a99ae0183cf594e86629

--- R/annotateErcc.R
+++ R/annotateErcc.R
@@ -1,8 +1,12 @@
-## if using something like RepBase, structure repeat classes hierarchically
-annotateRepeats <- function(res, repbase) {
+annotateErcc <- function(res) {
   
-  if (!grepl("RepBase", repbase)) stop("You must specify a RepBase assembly!")
-
+  ## so for ERCC spike-ins, repeats, and ENSEMBL transcripts, we can use (e.g.)
+  ## c("bundle", "name", "biotype") and always have something useful to offer.
+  data(ERCC)
+  names(ERCC)[1] <- c("bundle")
+  ERCC$name <- rownames(ERCC)
+  ERCC$biotype <- "SpikeIns"
   stop("Not done yet...")
+#  mcols(rowRanges(res))[match(rownames(ERCC), rownames(res)) ] <- 
 
 }
