33092f31fa936f5625b46042faa471845a66b445
ttriche <tim.triche@gmail.com>
2015-05-21 16:53:10 -0700

    debugging automated runs and adding an example

--- R/fetchKallisto.R
+++ R/fetchKallisto.R
@@ -18,8 +18,8 @@ fetchKallisto <- function(sampleDir=".", h5file="abundance.h5", checkRunInfo=T){
   if (bootstraps > 0) { 
     message("Found ", sampleDir, " bootstraps, summarizing...")
     boots <- do.call(cbind, h5read(hdf5, "bootstrap"))
-    res <- Matrix(cbind(rowMedians(boots), 
-                        rowMads(boots),
+    res <- Matrix(cbind(round(rowMedians(boots), 6), 
+                        round(rowMads(boots), 6),
                         h5read(hdf5, "aux/eff_lengths")),
                   dimnames=list(transcript=h5read(hdf5, "aux/ids"),
                                 c("est_count", "eff_length", "est_count_mad")))
