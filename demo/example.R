7df2c17fd071de4c61159d3c02ba4b4310f58378
ttriche <tim.triche@gmail.com>
2015-05-26 14:52:39 -0700

    remove excessive comments from example
    Former-commit-id: 3f7ebf681156e0ff94fc6b22959c8c056075e455

--- demo/example.R
+++ demo/example.R
@@ -16,16 +16,3 @@ tpm <- assays(merged)$est_count / assays(merged)$eff_length
 colnames(tpm) <- sub("Mr", "", colnames(tpm))
 heatmap(tpm[ rev(order(rowSds(tpm)))[1:100], ], 
         main="Repeat transcription, teratoma vs. normal")
-
-## look for gene-level differences 
-## reactome analysis on gene-level differences
-## gsea analysis on gene-level differences 
-## return a matrix from topTable for genes
-## eventually, use sleuth or a hier. model
-
-## look for transcript-level differences 
-## return a matrix from topTable for transcripts
-## eventually, use sleuth or a hier. model
-
-## add automated power calculations once this is reasonable 
-## eventually, control simultaneously for both with StructSSI, and use that.
