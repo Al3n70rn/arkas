ad3cacd4177873ec1a2398c8d39087fd7536e966
Anthony R. Colombo <anthonycolombo60@gmail.com>
2015-06-03 18:08:45 -0700

    added a merge transcriptome function which takes an input of fastaFiles as a vector, and then gunzip, cat, then gzips into mergedtranscptome.fa.gz file
    Former-commit-id: aec40133e12d3c6f7653bc77b25b522b72ae9ec2

--- demo/example.R
+++ demo/example.R
@@ -1,13 +1,17 @@
 library(artemis)
 
 fastaFiles <- c("ERCC.fa.gz", "Homo_sapiens.RepBase.humrep.v20_04.fa.gz")
-indexName <- indexKallisto(fastaFiles)$indexName
+
+#need to add the fastaPath into argument##
+indexName <- indexKallisto(fastaFiles, fastaPath)$indexName
 
 samples <- c("MrT", "MrN")
 names(samples) <- samples
 
 ## since these are lightweight runs, run them in parallel!
-results <- mclapply(samples, runKallisto, indexName=indexName)
+#results <- mclapply(samples, runKallisto, indexName=indexName)
+
+results
 outputPath <- unique(unlist(lapply(results, `[`, "outputPath")))
 
 ## merge 'em
