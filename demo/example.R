1ea6fa848cef10337430645beb1fab148e588236
ttriche <tim.triche@gmail.com>
2015-06-06 18:19:09 -0700

    revising example to use JSON inputs
    Former-commit-id: d4ca5f087ef859c31d64dec4186e5a3ef0549d14

--- demo/example.R
+++ demo/example.R
@@ -1,17 +1,29 @@
 library(artemis)
 
-fastaFiles <- c("ERCC.fa.gz", "Homo_sapiens.RepBase.humrep.v20_04.fa.gz")
-indexName <- indexKallisto(fastaFiles)$indexName
-
-samples <- c("MrT", "MrN")
-names(samples) <- samples
+jsonFile <- system.file("extdata", "Appsession.JSON", package="artemis")
+appSession <- fetchAppSession(jsonFile) ## autofill APPSESSION in paths
+samples <- c("MrT", "MrN") ## normally will be set by appSession
+names(samples) <- samples ## so that the column names get set 
+#
+# with(appSession, show(fastaFiles))
+# [1] "ERCC.fa.gz"                              
+# [2] "Homo_sapiens.RepBase.v20_05.humrep.fa.gz"
+#
+# with(appSession, show(fastaPath))
+# [1] "/Package_data/transcriptomes"
+#
+indexName <- with(appSession, 
+                  indexKallisto(fastaFiles=fastaFiles, 
+                                fastaPath=fastaPath)$indexName)
 
 ## since these are lightweight runs, run them in parallel!
-results <- mclapply(samples, runKallisto, indexName=indexName)
-outputPath <- unique(unlist(lapply(results, `[`, "outputPath")))
-
-## merge 'em
-merged <- mergeKallisto(samples, outputPath=outputPath)
+results <- mclapply(samples, 
+                    runKallisto,
+                    indexName=indexName,
+                    fastqPath=appSession$fastqPath,
+                    bootstraps=appSession$bootstraps, 
+                    outputPath=appSession$outputPath)
+merged <- mergeKallisto(samples, outputPath=appSession$outputPath)
 tpm <- assays(merged)$est_count / assays(merged)$eff_length
 colnames(tpm) <- sub("Mr", "", colnames(tpm))
 heatmap(tpm[ rev(order(rowSds(tpm)))[1:100], ], 
