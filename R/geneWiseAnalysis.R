96bb3bddde2245a1608db32029fa2e9b1f7a90f3
ttriche <tim.triche@gmail.com>
2015-07-01 06:40:37 -0700

    start updating gene-wise analysis for vignette
    Former-commit-id: 6740f0cedfd2894b61cd9a859a23ff9f825806a6

--- R/geneWiseAnalysis.R
+++ R/geneWiseAnalysis.R
@@ -5,14 +5,27 @@
 #' @param k           how many gene clusters to construct for comparison
 #' @param p.cutoff    where to set the p-value cutoff for such plots 
 #' @param ERCC        perform ERCC spike-in analysis as well? (default: TRUE)
+#' @param species     which species? (Homo.sapiens; FIX: get from transcriptome)
 #'
-geneWiseAnalysis <- function(res, categories=10, k=2, p.cutoff=0.05, ERCC=TRUE){
+#' @import limma
+#' @import FGNet
+#' @import ReactomePA
+#' @import Homo.sapiens
+#' @import Mus.musculus
+#' @import erccdashboard
+geneWiseAnalysis <- function(res, categories=10, k=2, p.cutoff=0.05, ERCC=TRUE,
+                             species=c("Homo.sapiens","Mus.musculus")) {
+
+  ## only two supported for now (would be simple to expand, though)
+  species <- match.arg(species)
+  commonName <- switch(species, Mus.musculus="mouse", Homo.sapiens="human")
 
   ## pull in erccdashboard if ERCC spike-ins were run
   if (ERCC == TRUE) {
     library(erccdashboard)
     ERCC_counts <- counts(ERCC(res))
-    ## plot them
+    ## FIXME: plot the ERCC controls for each sample
+    ## FIXME: remind the user that RUVg on ERCCs >> raw data >> ERCC-regressed
   }
 
   ## swap out for limma-trans or similar
@@ -25,9 +38,11 @@ geneWiseAnalysis <- function(res, categories=10, k=2, p.cutoff=0.05, ERCC=TRUE){
   top <- top[ abs(top$logFC) >= fold.cutoff, ] ## per SEQC recommendations
 
   ## ReactomePA for pathway analysis 
-  library(Homo.sapiens)
   topGenes <- rownames(top)
-  topGenes <- topGenes[topGenes %in% keys(Homo.sapiens, "ENTREZID")]
+
+  ## match species to map top genes to Entrez IDs 
+  library(species, character.only=TRUE)
+  topGenes <- topGenes[topGenes %in% keys(get(species), "ENTREZID")]
 
   ## overall
   enriched <- enrichPathway(gene=topGenes, 
@@ -50,9 +65,9 @@ geneWiseAnalysis <- function(res, categories=10, k=2, p.cutoff=0.05, ERCC=TRUE){
   enrichedGO <- list()
   enrichedRx <- list()
   for (i in names(genes)) {
-    enrichedGO[[i]] <- enrichGO(gene=genes[[i]], "human", readable=TRUE,
+    enrichedGO[[i]] <- enrichGO(gene=genes[[i]], commonName, readable=TRUE,
                                 qvalueCutoff=p.cutoff)
-    enrichedRx[[i]] <- enrichPathway(gene=genes[[i]], "human", readable=TRUE,
+    enrichedRx[[i]] <- enrichPathway(gene=genes[[i]], commonName, readable=TRUE,
                                      qvalueCutoff=p.cutoff)
   }
 
@@ -63,8 +78,20 @@ geneWiseAnalysis <- function(res, categories=10, k=2, p.cutoff=0.05, ERCC=TRUE){
     }
   }
 
-  ## GSEA
+  ## [Seq]GSEA... maybe better do it right, with UniProt/BioPAX structures?
+  ## Meanwhile, ReactomePA has facilities to do simple GSEA enrichment & plots
+  ## Gene sets from DSigDb (http://tanlab.ucdenver.edu/DSigDB/DSigDBv1.0/) may
+  ## be tremendously handy for looking at treatment-relevant diffexp/diffmeth.
+  ##
+  if (FALSE) { 
+    ## SeqGSEA/DsigDb analyses
+  }
 
+  ## KEGG pathway plots via FGNet
+  if (FALSE) { 
+    keggIds <- getTerms(feaAlzheimer, returnValue="KEGG")[[3]]
+    plotKegg("hsa05010", geneExpr=genesAlzExpr, geneIDtype="GENENAME")
+  }
 
   return(top)
 
