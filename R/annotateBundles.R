87bd6c0767290abc8a621f51f2ef560da2692b88
ttriche <tim.triche@gmail.com>
2015-07-02 11:47:06 -0700

    docs, exports, imports, and simplification
    Former-commit-id: 8b95f142aa861f0ba2a7f20ef2f497585952ba4f

--- R/annotateBundles.R
+++ R/annotateBundles.R
@@ -3,34 +3,20 @@
 #' 
 #' @param res     a SummarizedExperiment from mergeKallisto
 #' @param txome   a character string naming the txome, e.g. "EnsDb.Hsapiens.v79"
-#' @param cols    names of columns that must be present in order to proceed
 #'
 #' @export
-annotateBundles <- function(res, txome, 
-                            cols=c("bundle","name","egid","biotype")) {
+annotateBundles <- function(res, txome, ...) { 
 
-  if (txome == "ERCC") {
-    annotateErcc
-  } else if (!(grepl("ERCC", txome) | 
-        grepl("EnsDb", txome) | 
-        grepl("RepBase", txome))) {
+  if (!(grepl("(EnsDb|TxDb|Mus|Homo)", txome))) {
     stop(paste("Don't know how to process ", txome, " bundle annotations..."))
   } else { 
     library(txome, character.only=TRUE)
   }
 
-  txmap <- transcripts(get(txome), columns=c("tx_id","tx_biotype","entrezid"))
-  txmap <- txmap[which(txmap$tx_id %in% rownames(tpm) & txmap$entrezid != "")]
-  txmap <- txmap[!grepl(";", txmap$entrezid)] ## toss out multi-mapped ENSGs
-  names(txmap) <- txmap$tx_id ## so that the GRangesList makes sense later
-  mapByGene <- function(x) tapply(x[txmap$tx_id], txmap$entrezid, sum)
-  countsByGene <- apply(res$counts, 2, mapByGene) ## this is pretty fast, but
-  txsByGene <- GRangesList(split(txmap, txmap$entrezid)) ## why so slow?!
-  results <- list(SE=GenomicRanges::SummarizedExperiment(
-                       SimpleList(counts=countsByGene), 
-                                  rowData=txsByGene),
-                  tpmByTranscript=tpm, 
-                  txome=txome)
-  return(results)
+  if (!grepl("EnsDb", txome)) {
+    stop("Only EnsemblDb annotations are supported for now")
+  } else { 
+    annotateEnsembl(res, txome, ...)
+  }
 
 }
--- R/fetchRunInfo.R
+++ R/fetchRunInfo.R
@@ -1,7 +1,10 @@
 #' fetches the JSON-encoded run information for a Kallisto run
 #'
-#' @param ri character string specifying the name of the runinfo file
-#' 
+#' @param ri: character string specifying the name of the runinfo file
+#'
+#' @import jsonlite
+#'
+#' @export
 fetchRunInfo <- function(ri="run_info.json") { 
   fromJSON(ri)
 } 
