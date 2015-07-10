9c1775d041f1ac98e4644e9acd8edaf635803d43
ttriche <tim.triche@gmail.com>
2015-07-09 17:06:33 -0700

    ready to merge
    Former-commit-id: 856194071810b74817bbcc13c69f72383c1b6041

--- R/MultiAssayExperiment-class.R
+++ R/MultiAssayExperiment-class.R
@@ -1,11 +1,23 @@
 #' toy class for enforcing coherence between samples and perSampleMetadata
 #'
+#' @slot perSampleMetadata  metadata elements (e.g. QC) that must match colnames
+#' 
+#' @export
+#'
 setClass("MultiAssayExperiment",
          representation(perSampleMetadata="List"), # DataFrameList maybe?
          contains="RangedSummarizedExperiment")
+
+#' @describeIn MultiAssayExperiment
+#'
+#' @param object the MultiAssayExperiment (duh?)
+#' 
 setMethod("show", "MultiAssayExperiment",
           function(object) {
             callNextMethod()
             cat("perSampleMetadata:", names(object@perSampleMetadata), "\n")
           })
+#
+# the next method is actually the (only) critical method for the class.
+#
 # setValidity("MultiAssayExperiment", ...)  ## check the perSampleMetadata
