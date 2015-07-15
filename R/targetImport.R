71d1bcf83d5c13a2211774de5991282deb00b2cd
ttriche <tim.triche@gmail.com>
2015-07-15 13:44:05 -0700

    add import/conversion of TCGA, ICGC, etc. data
    Former-commit-id: 35444f540ba1126979625cfa8e60b7ac1313221e

---
+++ R/targetImport.R
@@ -0,0 +1,15 @@
+#' import RNAseq data from TARGET (isoform or gene level)
+#'
+#' @param counts    a matrix of TARGET counts (raw, median normalized, RPKM) 
+#' @param type      what type of data it is (default: figure out, ENSG vs ENST)
+#' 
+#' @return          a KallistoExperiment
+#'
+#' @seealso CountsAndFeaturesToKallistoExperiment
+#' 
+#' @export
+#'
+targetImport <- function(counts, ...) { 
+
+
+}
