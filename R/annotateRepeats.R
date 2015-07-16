555f2edf24b27546267f5d06df092d60450410b0
arcolombo <anthonycolombo60@gmail.com>
2015-07-16 22:51:26 +0000

    This has a anotation repeat for one entry only, vague annotation

--- R/annotateRepeats.R
+++ R/annotateRepeats.R
@@ -18,21 +18,35 @@ annotateRepeats <- function(kexp, repeatome, ...) {
    message("Annotating repeats supplied in ", repeatome, "...")
   #if (FALSE) { 
       repeatsDF<-as.data.frame(rptsByClass) #puts the rptsByClass GRangesList as a dataframe because names(rptsByClass) has <20 levels.  the DF is convenient
-   intersectRpts<-intersect(rownames(res)==repeatsDF$name) #grabs the intersected metadata between DF and rowData(res)
-   indexRpts<-which(repeatsDF$name==intersectRpts) #subsets repeatsDF
-   repeatsDF<-repeatsDF[indexRpts,]
+   i<-sapply(repeatsDF,is.factor)
+   repeatsDF[i]<-lapply(repeatsDF[i],as.character) #need to set rownames for repeatsDF
+    
+  intersectRpts<-intersect(rownames(kexp),repeatsDF$name) #grabs the intersected metadata between DF and rowData(res)
+   repeatsDF<-repeatsDF[repeatsDF$name %in% intersectRpts,] #subsets rows of repeatsDF to match the column of intersectRpts
 
-   repeatElement<-rep(repeatElement,nrow(repeatsDF))
-   names(repeatElement)<-repeatsDF$name
+   
+  repeatElement<-rep(repeatElement,length(intersectRpts)) #only 1 repeat per repeat
+  # repeatElement<-rep(repeatElement,nrow(repeatsDF)) #FIX THIS, how to filter repeats, and summarize them ? 
+   names(repeatElement)<-intersectRpts  #one repeat per entry FIX
+  # names(repeatElement)<-repeatsDF$name
    genome(repeatElement)<-unique(genome(rptsByClass)) #GRanges List loaded by RepeatsByClass.rda
    strand(repeatElement)<-repeatsDF$strand
-   
+   repeatElement$tx_biotype<-repeatsDF$class   
     
     # repeatElements <- rep(repeatElement, sum(grepl(repeats, rownames(kexp))))
    # names(repeatElements) <- grep(repeats, rownames(kexp), value=TRUE) 
    # features(kexp)[names(repeatElements)] <- repeatElements
+
+   #FIX ME : error because names(repeatElement) has 34,000 entries features(kexp) only has 1,215 entries.  sizes do not match
+  
      features(kexp)[names(repeatElement)]<-repeatElement
     return(kexp)
 #  }
 
 }
+
+#the problem I have , is for example.R the names(kexp) intersect repeatsDF is a list of names 693.  however in the GRanges list there is a total 5,000,000 entries, with 3,000,000 entries matchign this list.  
+  #hence the repeats, repeat alot.  how do you handle this ?
+  #this method in R, it just flags only the intersected names once, although there exist many repeats across many ranges, across many classes. 
+
+  
