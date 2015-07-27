b193d16555e5d81dac7a996a604c50330732b1b0
arcolombo <anthonycolombo60@gmail.com>
2015-07-27 18:38:38 +0000

    this has the erccAnalysis that plots ROC

--- R/erccAnalysis.R
+++ R/erccAnalysis.R
@@ -31,24 +31,25 @@ countsDF<-data.frame(Feature,counts)
 rownames(countsDF)<-c(1:nrow(countsDF))
 
 #formatting for erccDashboard processing
-colnames(countsDF)[2]<-"n_1"           
-colnames(countsDF)[3]<-"n_2"
-colnames(countsDF)[4]<-"n_4"
-colnames(countsDF)[5]<-"s_1"
-colnames(countsDF)[6]<-"s_2"
-colnames(countsDF)[7]<-"s_4"
+for(i in 2:ncol(countsDF)){
+ 
+   colnames(countsDF)[i]->tmp
+   tmp<-strsplit(tmp,"")[[1]]
+   tmp<-paste(tmp[1],tmp[2],sep="_")
+   colnames(countsDF)[i]<-tmp
+}
 
 
 #must be integer values
-countsDF[,2]<-as.integer(countsDF[,2])
-countsDF[,3]<-as.integer(countsDF[,3])
-countsDF[,4]<-as.integer(countsDF[,4])
-countsDF[,5]<-as.integer(countsDF[,5])
-countsDF[,6]<-as.integer(countsDF[,6])
-countsDF[,7]<-as.integer(countsDF[,7])
+for(i in 2:ncol(countsDF)){
+ 
+   countsDF[,i]<-as.integer(countsDF[,i])
+}
 
 #Must remove all the values for every row that contain a 0 for all columns
-which(countsDF[,2]!=0 & countsDF[,3]!=0 & countsDF[,4]!=0 & countsDF[,5]!=0 & countsDF[,6]!=0)->indx2
+colIntr<-colnames(countsDF)[(2:ncol(countsDF))]
+countsDF<-countsDF[!!rowSums(abs(countsDF[-c(1:1)])),]
+
 
 #not sure of input values used
 datType<-"count"
@@ -86,10 +87,4 @@ exDat <- erccROC(exDat)
 #multiple plot 1 pdf per page
 
 saveERCCPlots(exDat, plotsPerPg = "single", plotlist = exDat$Figures)
-
-
-
-
-
-
 }
