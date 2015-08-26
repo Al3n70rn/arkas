5b193b73505dcddea349015e76fafdd8363f054d
ttriche <tim.triche@gmail.com>
2015-08-25 19:39:18 -0700

    Automatic transcriptome detection is enabled.

--- R/fetchKallisto.R
+++ R/fetchKallisto.R
@@ -2,23 +2,29 @@
 #'
 #' @param sampleDir       character string: the path to h5/json files 
 #' @param h5file          character string: the file to read
-#' @param checkRunInfo    boolean: check run_info.json against HDF5 call? (TRUE)
 #' @param collapse        string: collapsing string for indices ("_mergedWith_")
 #'
 #' @import rhdf5 
 #' @import Matrix
-#' @import jsonlite 
+#'
 #' @importFrom matrixStats rowMads
 #' @importFrom matrixStats rowMedians
 #'
 #' @export
 fetchKallisto <- function(sampleDir=".", 
                           h5file="abundance.h5", 
-                          checkRunInfo=TRUE,
                           collapse="_mergedWith_", 
                           ...){
 
-  hdf5 <- paste0(path.expand(sampleDir), "/", h5file) 
+  hdf5 <- paste0(path.expand(sampleDir), .Platform$file.sep, h5file) 
+  if (!file.exists(hdf5)) { ## look for a named version of it... 
+    ## look and see if something similar exists, e.g. TARGET hdf5s
+    h5alt <- paste0(path.expand(sampleDir), .Platform$file.sep,
+                    list.files(sampleDir, pattern=h5file))
+    if (!file.exists(h5alt)) stop(paste("Could not locate", hdf5))
+    hdf5 <- h5alt
+  }
+
   bootstraps <- h5read(hdf5, "aux/num_bootstrap")
   ## if bootstraps are found, summarize them...
   
@@ -40,40 +46,53 @@ fetchKallisto <- function(sampleDir=".",
                                 c("est_counts", "eff_length")))
   }
 
-  ## ensure the information in run_info.json matches the hdf5 call
-  if (checkRunInfo) {
-    runinfo <- fetchRunInfo(sub("abundance.h5", "run_info.json", hdf5))
-    if (runinfo$call != h5read(hdf5, "aux/call")) {
-      stop("JSON run_info does not match hdf5 file! Something is likely wrong.")
-    } else {
-      ## parse index, get annotations, determine if bias corrected, etc.
-      fullruninfo <- .extractRunInfo(runinfo, collapse=collapse)
-    }
-
-    ## for sanity checking in mergeKallisto
-    for (i in names(fullruninfo)) {
-      attr(res, i) <- fullruninfo[[i]]
-    }
-
-  } else {
-    message("You have elected not to verify the run info for this sample.")
-    message("This means you will need to supply your own transcriptome IDs.")
-  }
-
+  runinfo <- .extractCall(h5read(hdf5, "aux/call"), collapse=collapse, ...)
+  for (i in names(runinfo)) attr(res, i) <- runinfo[[i]]
   return(res)
 
 }
 
-.extractRunInfo <- function(runinfo, collapse="_mergedWith_") {
+#' @import TxDbLite
+.extractCall <- function(callinfo, collapse="_mergedWith_") { # {{{
 
+  runinfo <- list()
   pop <- function(x) x[length(x)] 
-  popsplit <- function(x, y="/") pop(strsplit(x, y)[[1]])
-  callinfo <- strsplit(runinfo$call, " ", fixed=TRUE)[[1]]
-  index <- sub(".fa.kidx", "", popsplit(callinfo[ grep("^-i$", callinfo) + 1 ]))
-  runinfo$transcriptomes <- strsplit(index, collapse)[[1]]
-  runinfo$annotations <- sapply(runinfo$transcriptomes, 
-                                TxDbLite::getTxDbLiteName)
+  platsep <- .Platform$file.sep
+  popsplit <- function(x, y=platsep) pop(strsplit(x, y)[[1]])
+
+  tokens <- strsplit(callinfo, " ", fixed=T)[[1]]
+  indexname <- popsplit(tokens[grep("^-i$", tokens) + 1])
+
+  if (!grepl(collapse, callinfo)) {
+    ## sometimes we can split even without a proper collapsing string
+    ## requires a bit of ingenuity and a certain disdain for elegance
+    collapse <- "_" ## fallback...
+    ssub <- function(x, subs) {
+      for(s in names(subs)) x <- gsub(s, subs[s], x)
+      return(x)
+    }
+    subs <- c(Homo_sapiens="Hsapiens", 
+              Mus_musculus="Mmusculus", 
+              Rattus_norvegicus="Rnorvegicus",
+              "20_05"="RepBase2005",
+              "20_06"="RepBase2006",
+              "20_07"="RepBase2007",
+              "20_08"="RepBase2008",
+              "20_09"="RepBase2009")
+    unsubs <- names(subs)
+    names(unsubs) <- subs
+    unsubs["EnsV"] <- "" ## for TARGET
+    subs[".fa.idx"] <- "" ## for indices
+    subs[".fa.kidx"] <- "" ## for indices 
+    cleanUpIdx <- function(idx) ssub(idx, subs)
+    dirtyUpTxDb <- function(txDb) ssub(txDb, unsubs)
+    index <- cleanUpIdx(indexname)
+    runinfo$fastaFiles <- sapply(strsplit(index, collapse)[[1]], dirtyUpTxDb)
+  } else { 
+    index <- sub(".fa", "", sub(".idx", "", sub(".kidx", "", indexname)))
+    runinfo$fastaFiles <- strsplit(index, collapse)[[1]]
+  }
+  runinfo$transcriptomes <- sapply(runinfo$fastaFiles, getTxDbLiteName)
   runinfo$biascorrected <- any(grepl("--bias", callinfo))
   return(runinfo)
-
-}
+} # }}}
