6ed9e353cb4aca906b55330548a92741fb493bf1
ttriche <tim.triche@gmail.com>
2015-09-02 17:58:28 -0700

    automated annotation & transcriptome checking

--- R/mergeKallisto.R
+++ R/mergeKallisto.R
@@ -11,16 +11,18 @@
 #' FIXME: automatically determine which transcriptomes were used (in process!)
 #'
 #' @param outputDirs    character: directories holding Kallisto results (NULL)
-#' @param outputPath     character: base path to the outputDirs (default is .)
-#' @param covariates     data.frame or DataFrame: per-sample covariates (NULL)
-#' @param collapse       string: collapsing string for indices ("_mergedWith_")
-#' @param parallel       boolean: try to run the merge in parallel? (TRUE)
+#' @param outputPath    character: base path to the outputDirs (default is .)
+#' @param covariates    data.frame or DataFrame: per-sample covariates (NULL)
+#' @param annotate      boolean: automatically annotate the transcripts? (FALSE)
+#' @param collapse      string: collapsing string for indices ("_mergedWith_")
+#' @param parallel      boolean: try to run the merge in parallel? (TRUE)
 #'
 #' @export
 #'
 mergeKallisto <- function(outputDirs=NULL,
                           outputPath=".",
                           covariates=NULL,
+                          annotate=FALSE, 
                           collapse="_mergedWith_",
                           parallel=TRUE,
                           ...) { 
@@ -61,43 +63,38 @@ mergeKallisto <- function(outputDirs=NULL,
 
   stopifnot(all(sapply(asys, is, "matrix")))
   stopifnot(identical(colnames(asys[[1]]), colnames(asys[[2]])))
-  coldat <- DataFrame(ID=colnames(asys[[1]]))
+  coldat <- DataFrame(ID=outputDirs)
   rownames(coldat) <- coldat$ID
  
-  message("FIXME: split and automatically annotate the indexed transcriptomes!")
+  ktxs <- sapply(res, .extractTranscriptomeFromCall)
+  if (length(unique(ktxs)) > 1) {
+    print(table(ktxs))
+    stop("Your runs are against different transcriptomes! Aborting merge.")
+  } else {
+    message("Setting transcriptome automatically from Kallisto call string.")
+    transcriptomes <- attr(res[[1]], "transcriptomes")
+  }
 
-  ## FIXME: ensure they all used the same transcriptomes/aggregate-index
-  ## ktxs <- sapply(res, extractTranscriptomeFromCall)
-  ## 
-  ## if (length(unique(ktxs)) > 1) {
-  ##   stop("Your runs are against different transcriptomes! Aborting merge.")
-  ## } else {
-  ##   message("Setting transcriptome automatically from Kallisto call string.")
-  ##   transcriptome <- matchTranscriptome(unique(ktxs))
-  ## }
-
-  data(unannotatedTranscript)
+  data(unannotatedTranscript, package="artemis")
   rowdat <- rep(unannotatedTranscript, nrow(asys$est_counts))
   names(rowdat) <- rownames(asys$est_counts)
-  res <- KallistoExperiment(est_counts=asys$est_counts,
-                            eff_length=asys$eff_length,
-                            est_counts_mad=asys$est_counts_mad,
-                            transcriptomes=transcriptomes,
-                            kallistoVersion=kallistoVersion,
-                            covariates=coldat,
-                            features=rowdat,
-                            ...)
-  colnames(res) <- covariates(res)$ID 
-  if(!is.null(transcriptomes)) {
-    feats <- features(res)
-    mapped <- annotateBundles(res, transcriptomes)
+  kexp <- KallistoExperiment(est_counts=asys$est_counts,
+                             eff_length=asys$eff_length,
+                             est_counts_mad=asys$est_counts_mad,
+                             transcriptomes=transcriptomes,
+                             kallistoVersion=kallistoVersion,
+                             covariates=coldat,
+                             features=rowdat,
+                             ...)
+  colnames(kexp) <- kexp$ID
+  if(!is.null(transcriptomes) && annotate == TRUE) {
+    feats <- features(kexp)
+    mapped <- annotateFeatures(kexp, transcriptomes)
     feats[names(mapped)] <- mapped
-    features(res) <- feats
+    features(kexp) <- feats
   }
-  return(res)
+  return(kexp)
 
 }
 
-.extractTranscriptomeFromCall <- function(res) {
-
-}
+.extractTranscriptomeFromCall <- function(x) extractIndexName(attr(x, "call"))
