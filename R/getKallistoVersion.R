e7a93224136fd964f471b64e903ef1afc1fed69d
ttriche <tim.triche@gmail.com>
2015-10-05 12:32:14 -0700

    add versioning of Kallisto itself to index naming

---
+++ R/getKallistoVersion.R
@@ -0,0 +1,9 @@
+#' get the running version of Kallisto in the default path (if there is one)
+#' 
+#' @return  a string (the version of Kallisto that was found)
+#'
+#' @export
+#' 
+getKallistoVersion <- function() {
+  strpop(system2("kallisto", args="version", stdout=TRUE))
+}
