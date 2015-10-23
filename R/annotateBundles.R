896c5a57cd7f57ae59481edd1afa0129a183f9d4
Anthony R. Colombo <anthonycolombo60@gmail.com>
2015-10-23 11:10:04 -0700

    updating the annotation to include RepDb
    This is tested on my branch. works.

--- R/annotateBundles.R
+++ R/annotateBundles.R
@@ -21,9 +21,8 @@ annotateBundles <- function(kexp, transcriptomes, ...) {
     transcriptome <- transcriptomes
     if (grepl("EnsDb", ignore.case=TRUE, transcriptome)) {
       annotateEnsembl(kexp, transcriptome)
-      #  } else if (grepl("RepBase", ignore.case=TRUE, transcriptome)) {
-      #    ## this is on hold until repeatsByClass moves into its own package
-      #    annotateRepeats(kexp, transcriptome) 
+        } else if (grepl("RepDb", ignore.case=TRUE, transcriptome)) {
+         annotateRepeats(kexp, transcriptome) 
     } else if (grepl("ERCC", ignore.case=TRUE, transcriptome)) {
       annotateErcc(kexp, transcriptome)
     } else {       
