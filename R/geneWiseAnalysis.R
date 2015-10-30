01e821ae857c3d11dc134e42bc3c7c448f20c493
ttriche <tim.triche@gmail.com>
2015-10-30 16:41:58 -0700

    pre-merge commits

--- R/geneWiseAnalysis.R
+++ R/geneWiseAnalysis.R
@@ -1,15 +1,16 @@
 #' Downstream analysis of bundle-aggregated transcript abundance estimates.
 #'
-#' @param kexp        a KallistoExperiment or SummarizedExperiment-like object 
+#' @param kexp        a KallistoExperiment or SummarizedExperiment-like object
 #' @param design      a design matrix with 2nd coefficient as one to test
 #' @param p.cutoff    where to set the p-value cutoff for plots, etc. (0.05)
 #' @param fold.cutoff where to set the log2-FC cutoff for plots, etc. (1==2x)
 #' @param read.cutoff minimum read coverage (estimated) for a gene bundle 
-#' @param topheat     how many bundles to include in the cluster heatmaps? (100)
-#' @param species     which species? (Homo.sapiens; FIX: get from transcriptome)
+#' @param topheat     how many bundles to include in cluster heatmaps? (100)
+#' @param species     which species? (Homo.sapiens; FIX: get from TxDbLite)
 #' 
 #' @import edgeR 
 #' @import limma
+#' @import biomaRt
 #' @import ReactomePA 
 #' @import clusterProfiler
 #' @import Homo.sapiens
@@ -17,9 +18,8 @@
 #'
 #' @importFrom matrixStats rowSds 
 #' 
-#' 
 #' @details           If no design matrix is found, the function will look in 
-#'                    exptData(kexp)$design. If that too is empty it will fail.
+#'                    exptData(kexp)$design. If that too is empty it fails.
 #'                    There seems to be a bug in rendering Reactome plots, so 
 #'                    it may be necessary to do so manually:  
 #' \code{res <- geneWiseAnalysis(kexp, design, ...)} 
@@ -27,6 +27,8 @@
 #' \code{barplot(res$enriched, showCategory=10)}
 #'                    and 
 #' \code{plot(res$clusts)}
+#'           
+#'                    What really needs to happen is to break those out.
 #'
 #' @return            a list w/items design, voomed, fit, top, enriched,
 #'                                   Figures, scaledExprs, clusts, species,
@@ -56,11 +58,14 @@ geneWiseAnalysis <- function(kexp, design=NULL, how=c("cpm","tpm"),
   }
 
   ## only ones supported for now (would be simple to expand, though)
-  species <- match.arg(species) ## NOT to be confused with KEGG species ID
+   species <- match.arg(species, c("Homo.sapiens",
+                                       "Mus.musculus",
+                                       "Rattus.norvegicus")) ## NOT KEGG ID
   commonName <- switch(species, 
                        Mus.musculus="mouse", 
                        Homo.sapiens="human",
                        Rattus.norvegicus="rat")
+
 
   message("Fitting bundles...")
 
@@ -70,79 +75,50 @@ geneWiseAnalysis <- function(kexp, design=NULL, how=c("cpm","tpm"),
   res$top <- res$top[ abs(res$top$logFC) >= fold.cutoff, ] ## per SEQC
   topGenes <- rownames(res$top)
 
-  ## match species to map top genes to Entrez IDs 
-  # message("Matching species...")
-  # library(species, character.only=TRUE) ## can ignore this now 
-  # topGenes <- topGenes[topGenes %in% keys(get(species), "ENTREZID")]
+  if (FALSE) { # enrichment analysis needs to be separate
+    message("Performing Reactome enrichment analysis...")
+    message("Matching species...")
+    res$enriched <- enrichPathway(gene=converted[,2], 
+                                  qvalueCutoff=p.cutoff, 
+                                  readable=TRUE) 
 
-  ## overall
-  message("Performing Reactome enrichment analysis...")
-  message("Matching species...")
-  library(species, character.only=TRUE) ## can ignore this now 
-  enrich <- topGenes[topGenes %in% keys(get(species), "ENTREZID")]
-  res$enriched <- enrichPathway(gene=enrich, 
-                                qvalueCutoff=p.cutoff, 
-                                readable=TRUE) 
+    # adding res$Figures list object for multiplotting  
+    res$Figures <- list()
+    res$Figures$barplot <- barplot(res$enriched, 
+                                   showCategory=10, 
+                                   title="Overall Reactome enrichment")
 
-  #adding res$Figures list object for multiplotting  
-  res$Figures <- list()
-  res$Figures$barplot <- barplot(res$enriched, 
-                                 showCategory=10, 
-                                 title="Overall Reactome enrichment")
+    # limma is in terms of ensembl id
+    message("Performing clustered enrichment analysis...")
+    res$scaledExprs <- t(scale(t(res$voomed$E)))
+    
+    #finding scaled Expression in terms of entrez id
+    scaledConvertedID<-getBM(filters="ensembl_gene_id",
+                           attributes=c("ensembl_gene_id","entrezgene"),
+                           values=rownames(res$scaledExprs),
+                           mart=speciesMart)
 
-  ## cluster profiling within Reactome and/or GO 
-  message("Performing clustered enrichment analysis...")
-  res$scaledExprs <- t(scale(t(res$voomed$E[ enrich, ])))
-  ## turns out it's pretty easy: there's an "up" cluster, and a "down" cluster
-  clust <- cutree(hclust(dist(res$scaledExprs), method="ward"), k=2)
-  genes <- split(names(clust), clust)
-  names(genes) <- c("down", "up")
-  res$clusts <- compareCluster(geneCluster=genes, 
-                               fun="enrichPathway", 
-                               qvalueCutoff=p.cutoff)
+    message("clustering scaled expression in terms of entrez id ... ")
+    clust <- cutree(hclust(dist(res$scaledExprs), method="ward"), k=2)
+    genes <- split(names(clust), clust)
+      names(genes) <- c("down","up")
+      
+   res$clusts <- compareCluster(geneCluster=genes, 
+                                fun="enrichPathway", 
+                                 qvalueCutoff=p.cutoff)
 
-  #adding ggplot object for multiplotting
-  res$Figures$clusts <- plot(res$clusts) ## saving into Figures list
+    #adding ggplot object for multiplotting
+    res$Figures$clusts <- plot(res$clusts) ## saving into Figures list
 
-  #create a plot vector
-  #plots<-c("p1","p2")
+    # ReactomePA has facilities to do simple GSEA enrichment & plots
+    # DSigDb gene sets (http://tanlab.ucdenver.edu/DSigDB/DSigDBv1.0/) may
+    # be handy for looking at treatment-relevant differential expression.
 
-  #FIXME: don't do this, it's a huge mess and will produce unexpected results
-  #       instead, call the function on the res$object and plot that. 
-  # saveArtemisPlots(res, outName="geneWiseAnalysisJoined")
+  } 
 
-  ## up vs down genes
-  ## enrichedGO <- list()
-  ## enrichedRx <- list()
-  ## message("Performing GO analysis...")
-  ## for (i in names(genes)) {
-  ##   enrichedGO[[i]] <- enrichGO(gene=genes[[i]], commonName, readable=TRUE,
-  ##                               qvalueCutoff=p.cutoff)
-  ##   enrichedRx[[i]] <- enrichPathway(gene=genes[[i]], commonName, readable=T,
-  ##                                    qvalueCutoff=p.cutoff)
-  ## }
-  ## res$enrichedGO <- enrichedGO
-
-  ## for (i in names(genes)) {
-  ##   barplot(enrichedGO[[i]], showCategory=10, 
-  ##           title=paste("Gene ontologies", i))
-  ##   if (nrow(summary(enrichedRx[[i]])) > 0) {
-  ##     res$enrichedRx <- enrichedRx
-  ##     barplot(enrichedRx[[i]], showCategory=10, title=paste("Reactome", i))
-  ##   }
-  ## }
-  ## ReactomePA has facilities to do simple GSEA enrichment & plots
-  ##
-  ## Gene sets from DSigDb (http://tanlab.ucdenver.edu/DSigDB/DSigDBv1.0/) may
-  ## be tremendously handy for looking at treatment-relevant diffexp/diffmeth.
-  ##
-  ## EnrichmentBrowser is another recent package that may be tremendously handy
-  ##
-
-  ## for formatResults()
+  # for formatResults()
   res$features <- features(kexp)
   res$species <- species
   return(res)
 
 }
-
