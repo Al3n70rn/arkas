dfda9881ac4b5ab6a7fb3065ccfcc6a96abfdf0c
arcolombo <anthonycolombo60@gmail.com>
2015-11-26 12:30:02 -0800

    updating, adding ggFrame to be a parameter dependent on input Df
    Former-commit-id: 55b7ecbd300a577bb118281fca98e14ec46f6dd9

--- R/pcaPlot.R
+++ R/pcaPlot.R
@@ -3,7 +3,7 @@
 #' @import ggplot2
 #' @return a pca plot of an cpm, tpm, length, or median abs. deviations
 #' @export
-pcaPlot<-function(ggFrame) {
+pcaPlot<-function(ggFrame=ggInput) {
     firstInput<-attributes(ggFrame)$firstInput
     secondInput<-attributes(ggFrame)$secondInput
     assayInterested<-attributes(ggFrame)$assayInterested
@@ -15,7 +15,7 @@ pcaPlot<-function(ggFrame) {
   
 }#{{{main
 
-.plotGG<-function(ggFrame,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
+.plotGG<-function(ggFrame=ggInput,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
 
 plotTitle<-paste0("PC",firstInputComponent,".v.PC",secondInputComponent," ",assayInterested," Plot")
  outputGGPlot<-ggplot(data=ggFrame,aes(ggFrame[,1],ggFrame[,2],colour=lab))+geom_point()+ggtitle(plotTitle) + labs(x=paste0(colnames(ggFrame)[1],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[1] / sum(ggFrame$sdev))))) , y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[2] / sum(ggFrame$sdev))))) )
