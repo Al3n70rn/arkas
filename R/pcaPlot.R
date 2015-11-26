ab3df81aa8f8e6b0623cf3e7f1f86503a7b6e873
arcolombo <anthonycolombo60@gmail.com>
2015-11-26 11:51:56 -0800

    completing ggFrame creation

--- R/pcaPlot.R
+++ R/pcaPlot.R
@@ -1,84 +1,25 @@
 #'  plots pca of each kexp assay
-#' @param   kexpAssays, kallisto experiment assay
-#' @param   assayInterested, one assay of the kexp cpm,m.a.d, etc
-#' @param   firstComponent ,  the principal component you wish to compare with second 
-#' @param   secondComponent,  the principal component you wish to compare with first component 
+#' @param   ggFrame, a data frame of desired components to plot
 #' @import ggplot2
 #' @return a pca plot of an cpm, tpm, length, or median abs. deviations
 #' @export
-pcaPlot<-function(kexpAssays,assayInterested=c("cpm","tpm","length","mad"),firstComponent=c("first","second","third","fourth","fifth","sixth"),secondComponent=c("first","second","third","fourth","fifth","sixth")){
-
-firstComponent<-match.arg(firstComponent,c("first","second","third","fourth","fifth","sixth"))
-firstInput<-.principalSelection(firstComponent)
-secondComponent<-match.arg(secondComponent,c("first","second","third","fourth","fifth","sixth"))
-secondInput<-.principalSelection(secondComponent)
-
-assayInterested<-match.arg(assayInterested, c("cpm","tpm","length","mad"))
+pcaPlot<-function(ggFrame) {
+    firstInput<-attributes(ggFrame)$firstInput
+    secondInput<-attributes(ggFrame)$secondInput
+    assayInterested<-attributes(ggFrame)$assayInterested
     
-   
-     groups<-factor(colnames(kexpAssays))
-     pcaResult<-prcomp(t(kexpAssays))
-     ggFrame<-.ggData(pcaResult,firstInput,secondInput)
-     pcaPlot<-.plotGG(ggFrame,pcaResult,assayInterested,firstInputComponent=firstInput,secondInputComponent=secondInput)
-     return(list(ggFrame,pcaPlot))
+  
+     pcaPlot<-.plotGG(ggFrame,assayInterested,firstInputComponent=firstInput,secondInputComponent=secondInput)
+     return(pcaPlot)
    
   
 }#{{{main
 
-.plotGG<-function(ggFrame,pcaResult,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
+.plotGG<-function(ggFrame,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
 
 plotTitle<-paste0("PC",firstInputComponent,".v.PC",secondInputComponent," ",assayInterested," Plot")
- outputGGPlot<-ggplot(data=ggFrame,aes(ggFrame[,1],ggFrame[,2],colour=lab))+geom_point()+ggtitle(plotTitle) + labs(x=paste0(colnames(ggFrame)[1],sprintf(' (sd: %s%%)', round(100 * (pcaResult$sdev[1] / sum(pcaResult$sdev))))) , y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (pcaResult$sdev[2] / sum(pcaResult$sdev))))) )
+ outputGGPlot<-ggplot(data=ggFrame,aes(ggFrame[,1],ggFrame[,2],colour=lab))+geom_point()+ggtitle(plotTitle) + labs(x=paste0(colnames(ggFrame)[1],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[1] / sum(ggFrame$sdev))))) , y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[2] / sum(ggFrame$sdev))))) )
    outputGGPlot<- outputGGPlot+geom_text(data=ggFrame,aes(label=names),hjust=0.7,vjust=1)
  
 return(outputGGPlot)
-}
-
-
-.ggData<-function(pcaResult,firstInput,secondInput){
-     df<-as.data.frame(pcaResult$x)
-     df<-df[,c(firstInput,secondInput)]#takes PCinput1 and PCinput2
-     df<-cbind(df,substr(rownames(df),1,1))
-     df<-cbind(df,rownames(df))
-     colnames(df)[3:4]<-c("lab","names")
-     return(df)
-}
-
-
-.principalSelection<-function(inputComponent=NULL){
-
-    if(  grepl(inputComponent,"first",ignore.case=TRUE)=="TRUE"){
-      componentSelect<-1
-     return(componentSelect)
-    }
-
-   if( grepl(inputComponent,"second",ignore.case=TRUE)=="TRUE" ){
-     componentSelect<-2
-     return(componentSelect)
-    }
-
-    if( grepl(inputComponent,"third",ignore.case=TRUE)=="TRUE" ){
-    componentSelect<-3
-    return(componentSelect)
-    }
-
-    if( grepl(inputComponent,"fourth",ignore.case=TRUE)=="TRUE" ){
-    componentSelect<-4
-    return(componentSelect)
-    }
-    if( grepl(inputComponent,"fifth",ignore.case=TRUE)=="TRUE" ){
-    componentSelect<-5
-    return(componentSelect)
-    }
-   if( grepl(inputComponent,"sixth",ignore.case=TRUE)=="TRUE"){
-     componentSelect<-6
-    return(componentSelect)
-    }
-
-  else {
-     message("please select two components to compare first,second,third,fourth,fifth, or sixth ... ")
-    }
-}#{{{component Select
-
-
-
+} #{{{ plotGG main
