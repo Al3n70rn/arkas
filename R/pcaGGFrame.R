c540f023d9dcd6eab312f7e93e82c0685c44c553
arcolombo <anthonycolombo60@gmail.com>
2015-11-26 11:35:23 -0800

    adding pcaGGFrame to create ggplot data for plotting

--- R/pcaGGFrame.R
+++ R/pcaGGFrame.R
@@ -1,38 +1,25 @@
 #'  plots pca of each kexp assay
 #' @param   kexpAssays, kallisto experiment assay
-#' @param   assayInterested, one assay of the kexp cpm,m.a.d, etc
 #' @param   firstComponent ,  the principal component you wish to compare with second 
 #' @param   secondComponent,  the principal component you wish to compare with first component 
 #' @import ggplot2
-#' @return a pca plot of an cpm, tpm, length, or median abs. deviations
+#' @return a data Frame of the selected principal components used to pass into pcaPlot
 #' @export
-pcaPlot<-function(kexpAssays,assayInterested=c("cpm","tpm","length","mad"),firstComponent=c("first","second","third","fourth","fifth","sixth"),secondComponent=c("first","second","third","fourth","fifth","sixth")){
+pcaGGFrame<-function(kexpAssays,firstComponent=c("first","second","third","fourth","fifth","sixth"),secondComponent=c("first","second","third","fourth","fifth","sixth")){
 
 firstComponent<-match.arg(firstComponent,c("first","second","third","fourth","fifth","sixth"))
 firstInput<-.principalSelection(firstComponent)
 secondComponent<-match.arg(secondComponent,c("first","second","third","fourth","fifth","sixth"))
 secondInput<-.principalSelection(secondComponent)
 
-assayInterested<-match.arg(assayInterested, c("cpm","tpm","length","mad"))
-    
-   
-     groups<-factor(colnames(kexpAssays))
-     pcaResult<-prcomp(t(kexpAssays))
-     ggFrame<-.ggData(pcaResult,firstInput,secondInput)
-     pcaPlot<-.plotGG(ggFrame,pcaResult,assayInterested,firstInputComponent=firstInput,secondInputComponent=secondInput)
-     return(list(ggFrame,pcaPlot))
-   
-  
-}#{{{main
 
-.plotGG<-function(ggFrame,pcaResult,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
+    pcaResult<-prcomp(t(kexpAssays))
+    ggFrame<-.ggData(pcaResult,firstInput,secondInput)
 
-plotTitle<-paste0("PC",firstInputComponent,".v.PC",secondInputComponent," ",assayInterested," Plot")
- outputGGPlot<-ggplot(data=ggFrame,aes(ggFrame[,1],ggFrame[,2],colour=lab))+geom_point()+ggtitle(plotTitle) + labs(x=paste0(colnames(ggFrame)[1],sprintf(' (sd: %s%%)', round(100 * (pcaResult$sdev[1] / sum(pcaResult$sdev))))) , y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (pcaResult$sdev[2] / sum(pcaResult$sdev))))) )
-   outputGGPlot<- outputGGPlot+geom_text(data=ggFrame,aes(label=names),hjust=0.7,vjust=1)
- 
-return(outputGGPlot)
+
+return(ggFrame)
 }
+
 
 
 .ggData<-function(pcaResult,firstInput,secondInput){
@@ -40,10 +27,10 @@ return(outputGGPlot)
      df<-df[,c(firstInput,secondInput)]#takes PCinput1 and PCinput2
      df<-cbind(df,substr(rownames(df),1,1))
      df<-cbind(df,rownames(df))
-     colnames(df)[3:4]<-c("lab","names")
+     df<-cbind(df,pcaResult$sdev)
+     colnames(df)[3:5]<-c("lab","names","sdev")
      return(df)
 }
-
 
 .principalSelection<-function(inputComponent=NULL){
 
@@ -78,7 +65,7 @@ return(outputGGPlot)
   else {
      message("please select two components to compare first,second,third,fourth,fifth, or sixth ... ")
     }
-}#{{{component Select
+} #{{{component select
 
 
 
