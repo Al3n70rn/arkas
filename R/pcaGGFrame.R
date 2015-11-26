6f15240c3dee9e8b2de33b65949389ce879b6fd1
arcolombo <anthonycolombo60@gmail.com>
2015-11-26 11:51:56 -0800

    completing ggFrame creation
    Former-commit-id: ab3df81aa8f8e6b0623cf3e7f1f86503a7b6e873

--- R/pcaGGFrame.R
+++ R/pcaGGFrame.R
@@ -2,22 +2,24 @@
 #' @param   kexpAssays, kallisto experiment assay
 #' @param   firstComponent ,  the principal component you wish to compare with second 
 #' @param   secondComponent,  the principal component you wish to compare with first component 
+#' @param  assayInterested, the type of assay data, cpm, etc
 #' @import ggplot2
-#' @return a data Frame of the selected principal components used to pass into pcaPlot
+#' @return a data Frame of the selected principal components used to pass into pcaPlot, input attributes are under attributes(ggFrame) for each PC
 #' @export
-pcaGGFrame<-function(kexpAssays,firstComponent=c("first","second","third","fourth","fifth","sixth"),secondComponent=c("first","second","third","fourth","fifth","sixth")){
+pcaGGFrame<-function(kexpAssays,firstComponent=c("first","second","third","fourth","fifth","sixth"),secondComponent=c("first","second","third","fourth","fifth","sixth"), assayInterested=c("cpm","tpm","length","mad")) ){
 
-firstComponent<-match.arg(firstComponent,c("first","second","third","fourth","fifth","sixth"))
-firstInput<-.principalSelection(firstComponent)
-secondComponent<-match.arg(secondComponent,c("first","second","third","fourth","fifth","sixth"))
-secondInput<-.principalSelection(secondComponent)
+    firstComponent<-match.arg(firstComponent,c("first","second","third","fourth","fifth","sixth"))
+    firstInput<-.principalSelection(firstComponent)
+    secondComponent<-match.arg(secondComponent,c("first","second","third","fourth","fifth","sixth"))
+    secondInput<-.principalSelection(secondComponent)
+    assayInterested<-match.arg(assayInterested, c("cpm","tpm","length","mad"))
 
 
     pcaResult<-prcomp(t(kexpAssays))
     ggFrame<-.ggData(pcaResult,firstInput,secondInput)
     attributes(ggFrame)$firstInput<-firstInput
     attributes(ggFrame)$secondInput<-secondInput
-
+    attributes(ggFrame)$assayInterested<-assayInterested
 return(ggFrame)
 }
 
