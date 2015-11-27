74eecd4952d85815d03f8a130ef6065a127fc1e2
arcolombo <anthonycolombo60@gmail.com>
2015-11-26 23:23:11 -0800

    adding pcaPlotting, the input dataframe is saved into Global environment
    Former-commit-id: 0f7842db70dccc7b1a1df907fdc169206b47474d

--- R/pcaPlot.R
+++ R/pcaPlot.R
@@ -3,23 +3,36 @@
 #' @import ggplot2
 #' @return a pca plot of an cpm, tpm, length, or median abs. deviations
 #' @export
-pcaPlot<-function(ggFrame=ggInput) {
+pcaPlot<-function(ggFrame) {
+    
+    ggFrame<<-ggFrame #the ggFrame needs to be saved to the global environment
     firstInput<-attributes(ggFrame)$firstInput
     secondInput<-attributes(ggFrame)$secondInput
     assayInterested<-attributes(ggFrame)$assayInterested
-    
-  
-     pcaPlot<-.plotGG(ggFrame,assayInterested,firstInputComponent=firstInput,secondInputComponent=secondInput)
+    pcaPlot<-.plotGG(ggFrame,
+              assayInterested,
+              firstInputComponent=firstInput,
+               secondInputComponent=secondInput)
      return(pcaPlot)
    
   
 }#{{{main
 
-.plotGG<-function(ggFrame=ggInput,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
+.plotGG<-function(ggFrame,assayInterested, firstInputComponent=firstInput, secondInputComponent=secondInput){
 
 plotTitle<-paste0("PC",firstInputComponent,".v.PC",secondInputComponent," ",assayInterested," Plot")
- outputGGPlot<-ggplot(data=ggFrame,aes(ggFrame[,1],ggFrame[,2],colour=lab))+geom_point()+ggtitle(plotTitle) + labs(x=paste0(colnames(ggFrame)[1],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[1] / sum(ggFrame$sdev))))) , y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[2] / sum(ggFrame$sdev))))) )
-   outputGGPlot<- outputGGPlot+geom_text(data=ggFrame,aes(label=names),hjust=0.7,vjust=1)
+ outputGGPlot<-ggplot(data=ggFrame,aes_string(names(ggFrame)[1],
+               names(ggFrame)[2],
+               colour=names(ggFrame)[3]),
+               environment=environment()) 
+ outputGGPlot<- outputGGPlot +geom_point()+ggtitle(plotTitle) 
+  outputGGPlot<- outputGGPlot + labs(x=paste0(colnames(ggFrame)[1],
+                sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[1] / sum(ggFrame$sdev))))),
+                  y=paste0( colnames(ggFrame)[2],sprintf(' (sd: %s%%)', round(100 * (ggFrame$sdev[2] / sum(ggFrame$sdev))))) )
+   outputGGPlot<- outputGGPlot+geom_text(data=ggFrame,
+                  aes(label=names),
+                  hjust=0.7,
+                  vjust=1)
  
 return(outputGGPlot)
 } #{{{ plotGG main
