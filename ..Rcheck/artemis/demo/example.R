aec56e237b3cf925a2b54b09b6be499cd38946c3
arcolombo <anthonycolombo60@gmail.com>
2015-12-30 13:04:47 -0800

    updating...but these checks are wrong, need to scale back.
    Former-commit-id: d81223d8753125030755876c0826c14630c18553

---
+++ ..Rcheck/artemis/demo/example.R
@@ -0,0 +1,47 @@
+dataIsInstalled <- suppressPackageStartupMessages(require(artemisData))
+if(!dataIsInstalled){ 
+  message("You need to install the artemisData package.")
+  message("Try biocLite(\"ramsinghlab/artemisData\") then come back.")
+  stop("Go ahead, we'll wait!")
+} 
+library(matrixStats)
+
+message("Looking for Kallisto in ~/bin...")
+kallisto <- paste0(path.expand("~/bin"), "/kallisto")
+if(!file.exists(kallisto)) {
+  message("You do not seem to have kallisto installed.  We can't proceed.")
+} else {
+  message("Found it, proceeding...")
+}
+
+pathBase <- system.file("extdata", "", package="artemisData")
+fastaPath <- paste0(pathBase, "/fasta")
+fastqPath <- paste0(pathBase, "/fastq")
+samples <- c(MrN="MrN", MrT="MrT") ## normally set by appSession
+fastaFiles <- c( "ERCC.fa.gz", ## spike-in controls  
+                 "Homo_sapiens.RepBase.20_05.humrep.fa.gz", ## repeats 
+                 "Homo_sapiens.RepBase.20_05.humsub.fa.gz")  ## ALUs and such
+
+## build an index if it isn't already there (in artemisData, it is)
+indexName <- indexKallisto(fastaFiles=fastaFiles, fastaPath=fastaPath)$indexName
+
+## run pseudoalignments 
+library(parallel)
+results <- mclapply(samples, 
+                    runKallisto,
+                    indexName=indexName,
+                    fastqPath=fastqPath,
+                    fastaPath=fastaPath,
+                    bootstraps=100,
+                    outputPath=".")
+merged <- mergeKallisto(samples, outputPath=".")
+
+## plot repeat element txn using (counts/bootstrap MADs) as "effect size"
+topKbyMAD <- function(kexp, k=25) {
+  tpm(kexp)[rev(order(rowMeans(counts(kexp) / mad(kexp))))[1:k],]
+}
+pdf("ExamplePlot.pdf")
+heatmap(log1p(topKbyMAD(merged)), scale="none", 
+        col=colorRampPalette(c("white","red","darkred"))(255),
+        main="Repeat transcription, teratoma vs. normal")
+dev.off()
