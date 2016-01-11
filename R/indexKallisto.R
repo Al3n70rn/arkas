8a4d544aa06377b7cc223a1bee9b7b37afae119b
ttriche <tim.triche@gmail.com>
2016-01-11 11:26:43 -0800

    bump version, recognize new findDupes output

--- R/indexKallisto.R
+++ R/indexKallisto.R
@@ -30,6 +30,8 @@ indexKallisto <- function(fastaFiles, fastaPath, fastaTxDbLite=TRUE,
     ## Check the FASTA files for duplicate seqnames:
     dupes <- findDupes(fastaFiles)
     if (!is.null(dupes)) {
+      if (is(dupes, "data.frame") && sum(dupes$duplicates) == 0) break
+    } else {
       duplicatedSeqnames <- unique(dupes$seqname)
       message("There are duplicated sequence names in your FASTA files:")
       for (seqname in duplicatedSeqnames) { 
